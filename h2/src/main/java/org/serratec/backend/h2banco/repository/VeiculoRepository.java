29d98364c7c3d74bd1a60d15c1402830ec09862e
Beatriz Nolasco <89035781+trizisbea@users.noreply.github.com>
2021-11-02 17:11:11 -0300

    Delete h2 directory

--- h2/src/main/java/org/serratec/backend/h2banco/repository/VeiculoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.serratec.backend.h2banco.repository;
-
-import org.serratec.backend.h2banco.domain.Veiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-
-}
